b196055f80f4c4b7a993b8c7e68189c8f06734fc
Maksim11011 <mvbspb@bk.ru>
2024-07-19 16:32:40 +0300

    Добавил firebase изависимости  core,auth,email_validator,

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
-import 'package:everyday/pages/login_or_register.dart';
+import 'package:everyday/auth/login_or_register.dart';
+import 'package:everyday/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:everyday/generated/l10n.dart';
 import 'package:everyday/theme/theme_provider.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(ChangeNotifierProvider(
     create: (context) => ThemeProvider(),
     child: const MyApp(),
