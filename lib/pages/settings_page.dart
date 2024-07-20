2270938666d8a3f503e2dadbf9cecbc6696021da
Maksim11011 <mvbspb@bk.ru>
2024-07-20 16:39:09 +0300

    Добавил смену темы в настройки

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,4 +1,7 @@
+import 'package:everyday/theme/theme_provider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -7,11 +10,43 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: const Text('Настройи'),
         backgroundColor: Theme.of(context).colorScheme.background,
-        title: Text(
-          'Настройки',
-          style: TextStyle(color: Theme.of(context).colorScheme.inversePrimary),
-        ),
+      ),
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.secondary,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            margin: const EdgeInsets.only(left: 15, top: 5, right: 15),
+            padding: const EdgeInsets.all(15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // Темный режим
+                Text(
+                  'Темный режим',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                ),
+
+                // Переключатель
+                CupertinoSwitch(
+                  value: Provider.of<ThemeProvider>(context, listen: false)
+                      .isDarkMode,
+                  onChanged: (value) =>
+                      Provider.of<ThemeProvider>(context, listen: false)
+                          .toogleTheme(),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
